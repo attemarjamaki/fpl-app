8837325d74536392c851b7a58b2790c3f848ae7d
unknown <atte.marjamaki@arcada.fi>
2024-09-02 12:11:11 +0300

    landing page changes

--- app/page.js
+++ app/page.js
@@ -7,10 +7,22 @@ export default function Home() {
         <div className="flex flex-col justify-center text-center">
           <h1 className=" text-5xl font-bold">FPL Points Tracker</h1>
           <p className="mt-12 text-2xl md:w-4/5 w-full mx-auto">
-            Download your favorite YouTube videos and music in high-quality MP4
-            or MP3 effortlessly with our easy-to-use downloader.
+            Discover real-time insights and personalized analytics for your
+            fantasy football team with our dynamic, user-friendly web app
           </p>
           <Searchbar />
+        </div>
+        <div className="flex justify-center text-center mt-20">
+          <h2 className="text-3xl font-semibold ">How to find my FPL ID?</h2>
+        </div>
+        <div className="mockup-browser bg-base-300 border mt-10">
+          <div className="mockup-browser-toolbar">
+            <div className="input mx-6 !overflow-visible !w-4/5">
+              fantasy.premierleague.com/entry/
+              <span className=" border-4 border-red-600">123456</span>/event/3
+            </div>
+          </div>
+          <div className="bg-base-200 flex justify-center px-4 py-16 bg-[url('/images/pitch.svg')] bg-no-repeat bg-top bg-cover"></div>
         </div>
       </section>
     </>
