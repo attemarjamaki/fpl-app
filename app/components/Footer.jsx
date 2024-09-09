44a1c0ea2480a52463f751ff074e5106d897b883
unknown <atte.marjamaki@arcada.fi>
2024-09-10 01:58:25 +0300

    footer fixed for mobile

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="min-w-[600px]">
       <footer className="footer footer-center bg-neutral-700 text-neutral-200 text-md p-4">
         <aside>
           <p>Because FPL never sleeps—neither do we! ⚽🌙</p>
