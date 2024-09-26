05389061735651f886613ce69e857c2b47bf26b1
unknown <atte.marjamaki@arcada.fi>
2024-09-26 17:26:55 +0300

    footer changes

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="min-w-[600px]">
-      <footer className="footer footer-center bg-neutral-700 text-neutral-200 text-md p-6">
+      <footer className="footer footer-center bg-neutral-700 text-neutral-100 text-md p-6">
         <aside>
           <p>Because FPL never sleeps—neither do we! ⚽🌙</p>
           <p>
-            Copyright © {new Date().getFullYear()} - All rights reversed. Just
+            © {new Date().getFullYear()} FPL24.com - All rights reversed. Just
             kidding, they're reserved.
           </p>
         </aside>
