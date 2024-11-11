a8120517f4bf1dc73c1e35fa4ec2365b98348b1b
unknown <atte.marjamaki@arcada.fi>
2024-11-11 23:08:28 +0200

    changed cause haaland is shit

--- app/page.js
+++ app/page.js
@@ -11,8 +11,8 @@ export default function Home() {
             </span>
           </h1>
           <p className="mt-12 text-2xl md:w-4/5 w-full mx-auto">
-            A Fantasy Premier League tool to help you make bad transfers and
-            giving you the stress of not captaining Haaland in real time.
+            Your Fantasy Premier League tool to make boldly bad decisions and
+            track your underwhelming score in real-time!
           </p>
           <Searchbar />
         </div>
