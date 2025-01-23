4ed0429a6ef718a83cbcba9670501f8442e1fb90
unknown <atte.marjamaki@arcada.fi>
2025-01-23 15:38:07 +0200

    footer change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,10 +6,8 @@ const Footer = () => {
       <footer className="footer footer-center bg-neutral-700 text-neutral-100 text-md p-6">
         <aside>
           <p>Because FPL never sleeps—neither do we! ⚽🌙</p>
-          <p>
-            © {new Date().getFullYear()} FPL24.com - All rights reversed. Just
-            kidding, they're reserved.
-          </p>
+          <p>© {new Date().getFullYear()} FPL24.com | Lappers Digital</p>
+          <p>All rights reversed. Just kidding, they're reserved.</p>
         </aside>
       </footer>
     </div>
