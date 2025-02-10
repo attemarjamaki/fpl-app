2a985dbd00e2f08fdf6b493e4b2ff0ecea331681
unknown <atte.marjamaki@arcada.fi>
2025-02-10 22:38:33 +0200

    fix

--- app/captaincy/page.js
+++ app/captaincy/page.js
@@ -1,6 +1,11 @@
 "use client";
 
 import WheelSpinner from "@/components/WheelSpinner";
+import dynamic from "next/dynamic";
+
+const WheelSpinner = dynamic(() => import("@/components/WheelSpinner"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
