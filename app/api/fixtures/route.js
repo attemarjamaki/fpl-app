eb58fdfd6ae81573854bc5fef7930cc331fa82f0
unknown <atte.marjamaki@arcada.fi>
2025-09-13 16:34:11 +0300

    added force dynamic back

--- app/api/fixtures/route.js
+++ app/api/fixtures/route.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request) {
   try {
     const bootstrapResponse = await axios.get(
